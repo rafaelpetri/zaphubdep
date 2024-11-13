99c0a4f0064c9f132da5bf6383f98ae29de997b6
Rafael Petri <rafaelpetrimartins@hotmail.com>
2024-11-12 23:01:47 -0300

    Primeiro commit

--- install_packages.sh
+++ install_packages.sh
@@ -15,7 +15,7 @@ PACKAGES=(
     "docker-compose-plugin"
     "postgresql"
     "nodejs"
-    # Adicione mais pacotes conforme necessário
+    "snapd"         # Snapd para instalar o Certbot via Snap
 )
 
 # Função para instalar pacotes
@@ -33,4 +33,25 @@ for package in "${PACKAGES[@]}"; do
     install_package "$package"
 done
 
+# Instala o Certbot usando Snap
+if ! command -v certbot &> /dev/null; then
+    echo "Instalando Certbot..."
+    sudo snap install core
+    sudo snap refresh core
+    sudo snap install --classic certbot
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+else
+    echo "Certbot já está instalado."
+fi
+
+# Pergunta os subdomínios
+read -p "Digite o primeiro subdomínio (exemplo: subdominio1.dominio.com): " SUBDOMAIN1
+read -p "Digite o segundo subdomínio (exemplo: subdominio2.dominio.com): " SUBDOMAIN2
+
+# Configuração do Certbot para os subdomínios
+echo "Gerando certificados SSL para $SUBDOMAIN1 e $SUBDOMAIN2..."
+
+sudo certbot --nginx -d "$SUBDOMAIN1" -d "$SUBDOMAIN2" --non-interactive --agree-tos -m seu-email@exemplo.com
+
+echo "Certificados SSL gerados com sucesso para $SUBDOMAIN1 e $SUBDOMAIN2."
 echo "Instalação concluída."
